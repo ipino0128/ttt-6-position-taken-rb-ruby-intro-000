489e73a67a76555da09e89b86d015dcb194819c8
ipino0128@gmail.com <ipino0128@gmail.com>
2018-08-01 01:38:28 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def position_taken?(board, index)
-  if board[index] == " "
+  if board[index] == " " || board[index] == ""
     false
 end
 end
